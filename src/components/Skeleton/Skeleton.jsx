3c6ecb07e888f8493f94b569e918aac294c2a61a
Liliia Danylenko <liliia.danylenko.dev@gmail.com>
2024-10-30 22:45:06 +0200

    added skeleton for news and banner

---
+++ src/components/Skeleton/Skeleton.jsx
@@ -0,0 +1,19 @@
+import s from './Skeleton.module.css'
+
+const Skeleton = ({ count = 1, type = 'banner' }) => {
+  return (
+    <>
+      {count > 1 ? (
+        <ul className={s.list}>
+          {[...Array(count)].map((_, i) => (
+            <li key={i} className={type === 'banner' ? s.banner : s.item}></li>
+          ))}
+        </ul>
+      ) : (
+        <li className={type === 'banner' ? s.banner : s.item}></li>
+      )}
+    </>
+  )
+}
+
+export default Skeleton
