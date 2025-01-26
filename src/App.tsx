3c665cd658954c080f02e07f53556eb53e0f269b
Liliia Danylenko <liliia.danylenko.dev@gmail.com>
2025-01-26 22:17:29 +0200

    add context and change theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import Header from "./components/Header/Header";
-import Main from "./pages/Main/Main";
+import Header from './components/Header/Header';
+import Main from './pages/Main/Main';
+import { useTheme } from './context/ThemeContext';
 
 function App() {
+  const { isDark } = useTheme();
+
   return (
-    <>
+    <div className={`app ${isDark ? 'dark' : 'light'}`}>
       <Header />
       <Main />
-    </>
+    </div>
   );
 }
 
-export default App
+export default App;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,21 @@
-import { formatDate } from "../../helpers/formatDate"
-import s from './Header.module.css'
+import { themeIcons } from '../../assets';
+import { formatDate } from '../../helpers/formatDate';
+import s from './Header.module.css';
+import { useTheme } from '../../context/ThemeContext';
 
 const Header = () => {
-  return (
-    <header className={s.header}>
-      <h1 className={s.title}>NEWS</h1>
-      <p className={s.date}>{formatDate(new Date())}</p>
-    </header>
-  )
-}
+  const { isDark, toggleTheme } = useTheme();
 
-export default Header
+  return (
+    <header className={`${s.header} ${isDark ? s.dark : s.light}`}>
+      <div className={s.info}>
+        <h1 className={s.title}>NEWS</h1>
+        <p className={s.date}>{formatDate(new Date())}</p>
+      </div>
+
+      <img src={isDark ? themeIcons.light : themeIcons.dark} width={30} alt="theme switcher" onClick={toggleTheme} />
+    </header>
+  );
+};
+
+export default Header;
