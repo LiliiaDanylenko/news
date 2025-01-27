d164568f7ab0fa989fe2d9d0375fa401e882d792
Liliia Danylenko <liliia.danylenko.dev@gmail.com>
2025-01-27 21:47:10 +0200

    add redux toolkit and rtk query

--- src/components/NewsFilters/NewsFilters.tsx
+++ src/components/NewsFilters/NewsFilters.tsx
@@ -1,6 +1,7 @@
-import { getCategories } from '../../api/apiNews';
-import { useFetch } from '../../helpers/hooks/useFetch';
-import { CategoriesApiResponse, IFilters } from '../../interfaces';
+import { IFilters } from '../../interfaces';
+import { useAppDispatch } from '../../store';
+import { useGetCategoriesQuery } from '../../store/services/newsApi';
+import { setFilters } from '../../store/slices/newsSlice';
 import Categories from '../Categories/Categories';
 import Search from '../Search/Search';
 import Slider from '../Slider/Slider';
@@ -8,25 +9,32 @@ import s from './NewsFilters.module.css';
 
 interface Props {
   filters: IFilters;
-  changeFilter: (key: string, value: string | number | null) => void;
 }
 
-const NewsFilters = ({ filters, changeFilter }: Props) => {
-  const { data: dataCategories } = useFetch<CategoriesApiResponse, null>(getCategories);
+const NewsFilters = ({ filters }: Props) => {
+  const { data } = useGetCategoriesQuery(null);
+  const dispatch = useAppDispatch();
 
   return (
     <div className={s.filters}>
-      {dataCategories && (
+      {data && (
         <Slider>
           <Categories
-            categories={dataCategories.categories}
+            categories={data.categories}
             selectedCategory={filters.category}
-            setSelectedCategory={(category) => changeFilter('category', category)}
+            setSelectedCategory={(category) =>
+              dispatch(setFilters({ key: 'category', value: category }))
+            }
           />
         </Slider>
       )}
 
-      <Search keywords={filters.keywords} setKeywords={(keywords) => changeFilter('keywords', keywords)} />
+      <Search
+        keywords={filters.keywords}
+        setKeywords={(keywords) =>
+          dispatch(setFilters({ key: 'keywords', value: keywords }))
+        }
+      />
     </div>
   );
 };
